53a8e9fef3e6618ec114e74e655571cbda8ac59e
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2019-01-21 23:53:37 +0100

    update ckeditor_file

--- lib/la_potiniere_web/admin_potiniere_web/controllers/ckeditor_file_controller.ex
+++ lib/la_potiniere_web/admin_potiniere_web/controllers/ckeditor_file_controller.ex
@@ -5,7 +5,10 @@ defmodule LaPotiniereWeb.AdminPotiniereWeb.CkeditorFileController do
   alias LaPotiniere.CkeditorFiles.CkeditorFile
   
   def create(conn, _params) do
-    IO.inspect "-=-=-=-=-=-=-=-#{_params}"
-    #case CkeditorFiles.create_ckeditor_file()
+    ckeditor_file = %{"ckeditor_file" => %{"file" => _params["upload"]}}
+    #case CkeditorFiles.create_ckeditor_file(ckeditor_file["ckeditor_file"]) do
+      #{:ok, ckeditor_file} ->
+      #{:error, %Ecto.Changeset{} = changeset} ->
+    #end
   end
 end
