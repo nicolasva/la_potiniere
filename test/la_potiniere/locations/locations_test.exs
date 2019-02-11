24a974569255b58fb80c6f58bb98ade9fbe4c43a
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2019-02-11 14:02:28 +0100

    fix unit test location

--- test/la_potiniere/locations/locations_test.exs
+++ test/la_potiniere/locations/locations_test.exs
@@ -8,20 +8,21 @@ defmodule LaPotiniere.LocationsTest do
 
 
     @valid_attrs %{address: "30 Rue François Arago", cp_postal: "93500", city: "PANTIN"}
-  
+
 
     def location_fixture(attrs \\ %{}) do
+      LaPotiniere.Roles.create_role(%{role_value: 0})
+      user = LaPotiniere.Users.create_user(%{name: "VANDENBOGAERDE", username: "nicolasva", password: nil, email: "nicolas.vandenbogaerde@gmail.com"})
+      Map.put(@valid_attrs, :users, user)
       {:ok, location} =
         attrs
         |> Enum.into(@valid_attrs)
-        Locations.create_location(attrs, user)
+        |> Locations.create_location()
 
       location
     end
 
     test "list_locations/0 returns all locations" do
-      LaPotiniere.Roles.create_role(%{role_value: 0})
-      user = LaPotiniere.Users.create_user(%{name: "VANDENBOGAERDE", username: "nicolasva", password: nil, email: "nicolas.vandenbogaerde@gmail.com"})
       location = location_fixture()
       assert Locations.list_locations() == [location]
     end
