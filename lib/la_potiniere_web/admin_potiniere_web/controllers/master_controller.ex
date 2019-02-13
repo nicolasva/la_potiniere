6fa215093529d613b2ad2132efbde6b27272de43
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2019-02-13 23:44:13 +0100

    update master_controller for content event and photo

--- lib/la_potiniere_web/admin_potiniere_web/controllers/master_controller.ex
+++ lib/la_potiniere_web/admin_potiniere_web/controllers/master_controller.ex
@@ -3,18 +3,38 @@ defmodule LaPotiniereWeb.AdminPotiniereWeb.MasterController do
 
   alias LaPotiniere.{
     Contents,
+    Events,
+    Photos,
     Masters
   }
 
-  alias LaPotiniere.Contents.Content
   alias LaPotiniere.Masters.Master
 
-  def index(conn, %{"menu_id" => menu_id, "content_id" => content_id}) do
-    if content_id do
-      changeset = Contents.get_content!(content_id)
+  def index(conn, _params) do
+    if _params["content_id"] do
+      changeset = Contents.get_content!(_params["content_id"])
     end
+
+    if _params["event_id"] do
+      changeset = Events.get_event!(_params["event_id"])
+    end
+
+    if _params["photo_id"] do
+      changeset = Photos.get_photo!(_params["photo_id"])
+    end
+
     Masters.create_master(changeset)
-    redirect(conn, to: Routes.admin_menu_content_path(conn, :index, menu_id))
+    if _params["content_id"] do
+      redirect(conn, to: Routes.admin_menu_content_path(conn, :index, _params["menu_id"]))
+    end
+
+    if _params["event_id"] do
+      redirect(conn, to: Routes.admin_menu_event_path(conn, :index, _params["menu_id"]))
+    end
+
+    if _params["photo_id"] do
+      redirect(conn, to: Routes.admin_menu_photo_path(conn, :index, _params["menu_id"]))
+    end
       #{:ok, master} ->
         #  conn
         #|> put_flash(:info, "Cette page à bien été enregistré comme page par défaut")
