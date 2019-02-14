5e0f2fc180c25a048a85ff6ae2d7f1d0058f6912
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2019-02-14 15:21:33 +0100

    update menu_view

--- lib/la_potiniere_web/admin_potiniere_web/views/menu_view.ex
+++ lib/la_potiniere_web/admin_potiniere_web/views/menu_view.ex
@@ -1,3 +1,11 @@
 defmodule LaPotiniereWeb.AdminPotiniereWeb.MenuView do
   use LaPotiniereWeb.AdminPotiniereWeb, :view
+
+  def is_menu_master(master_menu, current_menu) do
+    if master_menu.id == current_menu.id, do: "Contenu par défaut afficher de ce menu"
+  end
+
+  def menu_user(menu) do
+    if menu.user, do: "#{menu.user.name} #{menu.user.username}<#{menu.user.email}>"
+  end
 end
