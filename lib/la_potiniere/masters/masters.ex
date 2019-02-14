a6f22a531c32961a709a80d6db3b1282d75205a1
Nicolas VANDENBOGAERDE <nicolas.vandenbogaerde@gmail.com>
2019-02-14 12:15:45 +0100

    update masters.ex

--- lib/la_potiniere/masters/masters.ex
+++ lib/la_potiniere/masters/masters.ex
@@ -18,12 +18,21 @@ defmodule LaPotiniere.Masters do
     Repo.all(Master) |> Repo.preload(:events) |> Repo.preload(:pictures) |> Repo.preload(:contents)
   end
 
+  def get_master!(id), do: Repo.get!(Master, id)
+
   def create_master(parent) do
     delete_all()
     changeset = 
       parent
       |>Ecto.build_assoc(:master)
       |> Repo.insert()
+  end
+
+  def menu_master! do
+    master = get_master!(7) |> Repo.preload([[content: :menu], [photo: :menu], [event: :menu]])
+    if master.content, do: master.content.menu
+    if master.event, do: master.event.menu
+    if master.photo, do: master.photo.menu
   end
 
   defp delete_all do
